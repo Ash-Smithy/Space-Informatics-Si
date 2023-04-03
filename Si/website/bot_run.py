b3d103f4c0dcfc030474bd30b881c001e9ba2455
Ash <80884527+Ash-Smithy@users.noreply.github.com>
2023-04-04 01:23:47 +0530

    comment updates

--- Si/website/bot_run.py
+++ Si/website/bot_run.py
@@ -8,18 +8,22 @@ from tensorflow import keras
 import nltk
 from nltk.stem import WordNetLemmatizer
 
+# to reduce a word to its stem/base form
 lemmatizer = WordNetLemmatizer()
-intents = json.loads(open('Si/website/bot_intents.json').read())
+intents = json.loads(open('Si/website/bot_intents.json').read()) #to load JSON 
 
-words = pickle.load(open('words.pkl','rb'))
+#loading required files and model
+words = pickle.load(open('words.pkl','rb')) #rb = reading binary mode
 classes = pickle.load(open('classes.pkl','rb'))
-model = keras.models.load_model('chatbotmodel.h5')
+model = keras.models.load_model('chatbotmodel.h5') 
 
+#tokenize input and reduce to stem
 def clean_up_sentence(sentence):
     sentence_words = nltk.word_tokenize(sentence)
     sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
     return sentence_words
 
+#convert a sentence into a bage of words (list 0,1 to know if word is there or not)
 def bag_of_words(sentence):
     sentence_words = clean_up_sentence(sentence)
     bag = [0] * len(words)
@@ -29,21 +33,24 @@ def bag_of_words(sentence):
                 bag[i] = 1
     return np.array(bag)
 
+#to predict the class using the model
 def predict_class(sentence):
     bow = bag_of_words(sentence)
     res = model.predict(np.array([bow]))[0]
-    ERROR_THRESHOLD = 0.25
-    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]
-
+    ERROR_THRESHOLD = 0.25 #  25% for uncertaininty
+    results = [[i,r] for i, r in enumerate(res) if r > ERROR_THRESHOLD]  #enumerate is to get the class
+   
     results.sort(key=lambda x: x[1], reverse=True)
     return_list = []
     for r in results:
         return_list.append({'intent':classes[r[0]], 'probability': str(r[1])})
     return return_list
 
+#takes the predicted intents
 def get_response(intents_list, intents_json):
     tag = intents_list[0]['intent']
     list_of_intents = intents_json['intents']
+    #generate random response
     for i in list_of_intents:
         if i['tag'] == tag:
             result = random.choice(i['responses'])
@@ -52,6 +59,7 @@ def get_response(intents_list, intents_json):
             result = "I do not understand"
     return result
 
+#function to take user input, generate response and return the generated response
 def result(val):
     val = val.lower()
     while True:
--- Si/website/bot_train.py
+++ Si/website/bot_train.py
@@ -11,12 +11,14 @@ from nltk.stem import WordNetLemmatizer
 # to reduce a word to its stem/base form
 lemmatizer = WordNetLemmatizer()
 
+#to load JSON 
 intents = json.loads(open('Si/website/bot_intents.json').read())
 words = []
 classes = []
 documents = []
 ignore_letters = ['?','!','.',',']
 
+#to extract words from pattens and append to a list
 for intent in intents['intents']:
     for pattern in intent['patterns']:
         word_list = nltk.word_tokenize(pattern) #tokenize is used to break down the words
@@ -25,44 +27,54 @@ for intent in intents['intents']:
         if intent['tag'] not in classes:
             classes.append(intent['tag'])
 
+#create list of words for chatbot
 words = [lemmatizer.lemmatize(word) for word in words if word not in ignore_letters]
-words = sorted(set(words))
+words = sorted(set(words)) #to eliminate dumplicates
 
+#create list of classes for chatbot to recognise
 classes = sorted(set(classes))
 
-pickle.dump(words, open('words.pkl', 'wb'))
+#dump the created words,classes into pickle files (pickle files convert objects into byte streams)
+pickle.dump(words, open('words.pkl', 'wb')) #wb - write binary (to write into the file)
 pickle.dump(classes, open('classes.pkl','wb'))
+
 
 training = []
 output_empty = [0] * len(classes)
 
+#Converting words to numerical values for the neural network
 for document in documents:
     bag = []
     word_patterns = document[0]
     word_patterns = [lemmatizer.lemmatize(word.lower()) for word in word_patterns]
+    #to give value if word occurs in the pattern
     for word in words:
         bag.append(1) if word in word_patterns else bag.append(0)
 
 
-    output_row = list(output_empty)
+    output_row = list(output_empty) #copying the list
     output_row[classes.index(document[1])] = 1
     training.append([bag, output_row])
 
+#shuffling the training data to randomize order
 random.shuffle(training)
 training = np.array(training)
 
+#split training data into input X and output Y
 train_x = list(training[:,0])
 train_y = list(training[:,1])
 
+#Building the network
 model = keras.models.Sequential()
-model.add(keras.layers.Dense(128, input_shape=(len(train_x[0]),),activation='relu'))
-model.add(keras.layers.Dropout(0.5))
+model.add(keras.layers.Dense(128, input_shape=(len(train_x[0]),),activation='relu')) #input layer
+model.add(keras.layers.Dropout(0.5)) #to prevent overfitting
 model.add(keras.layers.Dense(64, activation='relu'))
 model.add(keras.layers.Dropout(0.5))
-model.add(keras.layers.Dense(len(train_y[0]), activation='softmax'))
-sgd = keras.optimizers.SGD(learning_rate=0.01, weight_decay= 1e-6, momentum=0.9, nesterov=True)
-model.compile(loss= 'categorical_crossentropy', optimizer=sgd, metrics= ['accuracy'])
+model.add(keras.layers.Dense(len(train_y[0]), activation='softmax')) #softmax scales numbers/logits into probabilities
+sgd = keras.optimizers.SGD(learning_rate=0.01, weight_decay= 1e-6, momentum=0.9, nesterov=True)  # 1e-6 = 0.000001 
+model.compile(loss= 'categorical_crossentropy', optimizer=sgd, metrics= ['accuracy']) #SGD is an algorithm which helps in calculating the gradients for variable to create new values for the variable
 
+#fitting the model
 hist = model.fit(np.array(train_x),np.array(train_y),epochs=200, batch_size=5,verbose=1)
 model.save('chatbotmodel.h5', hist)
 print("done")
